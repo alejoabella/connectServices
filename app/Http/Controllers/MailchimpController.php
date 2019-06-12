4bd45622223b44c5b1b767df429167a352412b59
Alejo Abella <alejosebastianabella@gmail.com>
2019-06-11 23:18:44 -0300

    Mailchimp OOP

--- app/Http/Controllers/MailchimpController.php
+++ app/Http/Controllers/MailchimpController.php
@@ -19,88 +19,24 @@ class MailchimpController extends Controller
 
     public function login()
     {   
-        $baseUri = "https://login.mailchimp.com/oauth2/";
-        return redirect($baseUri . 'authorize?response_type=code&client_id=' . env('MAILCHIMP_API_CLIENT_ID') . '&redirect_uri=' . env('MAILCHIMP_API_REDIRECT_URI'));
+        return redirect($this->mailchimp->prepareUrl());
     }
 
     public function response(Request $request)
     {
         
-        $this->mailchimp->getToken($request->input('code'));
-        $this->mailchimp->getMetadata($apiKey);
+        $accessToken = $this->mailchimp->getToken($request->input('code'));
+        $this->mailchimp->getMetadata($accessToken);
     }
 
-    public function metadata()
+    public function showLists()
     {
-
-        
-        // Store metadata in DB
-        dd($response->getBody()->getContents());
-
+        $lists = $this->mailchimp->getLists($accessToken);
     }
 
-
-    public function getLists()
+    public function getSelectedLists()
     {
-        $client = new Client([
-            'base_uri' => 'https://us20.api.mailchimp.com/3.0/',
-            'timeout'  => 2.0,
-        ]);
-
-        $response = $client->request('GET', 'lists', [
-            'headers' => [
-                'Authorization' => 'apikey ' . env('MAILCHIMP_API_KEY')
-            ]
-        ]);
-
-        // Store List in DB
-        dd($response->getBody()->getContents());
-    }
-
-    public function getMembersByListId()
-    {
-
-        $client = new Client([
-            'base_uri' => 'https://us20.api.mailchimp.com/3.0/',
-            'timeout'  => 0,
-        ]);
-        
-        // Get all members
-        $offset = 0;
-        $count = 1000;
-
-        do {
-
-            $response = $client->request('GET', 'lists/828d1335f5/members?offset=' . $offset . '&count=' . $count, [
-                'headers' => [
-                    'Authorization' => 'apikey ' . env('MAILCHIMP_API_KEY')
-                ]
-            ]);
-
-            $r = json_decode($response->getBody()->getContents())->members;
-
-            $total = count($r);
-
-            foreach ($r as $key) {
-                echo $key->email_address . "<br>";
-            }
-
-            $offset += 1000;
-            
-        } while ($total >= 1000);
-
-
-/*         DB::table('users')->insert([
-            ['email' => 'taylor@example.com'],
-            ['email' => 'dayle@example.com']
-        ]); */
-        // Store all members in DB
-
-    }
-
-    public function processList()
-    {
-
+        $members = $this->mailchimp->getMembers($accessToken);
     }
 
 }
--- app/Repositories/Mailchimp.php
+++ app/Repositories/Mailchimp.php
@@ -3,8 +3,17 @@
 namespace App\Repositories;
 
 use GuzzleHttp\Client;
+use function GuzzleHttp\json_decode;
 
 class Mailchimp {
+
+    public function prepareUrl()
+    {
+        $baseUri = 'https://login.mailchimp.com/oauth2/authorize';
+        $clientId = env('MAILCHIMP_API_CLIENT_ID');
+        $redirectUri = env('MAILCHIMP_API_REDIRECT_URI');
+        return $baseUri . '?response_type=code&client_id=' . $clientId . '&redirect_uri=' . $redirectUri;
+    }
 
     public function getToken($code)
     {
@@ -23,10 +32,10 @@ class Mailchimp {
             ]
         ]);
 
-        return $token = $response->getBody()->getContents();
+        return json_decode($response->getBody()->getContents())->access_token;
     }
 
-    public function getMetada($apiKey)
+    public function getMetadata($accessToken)
     {
         $client = new Client([
             'base_uri' => 'https://login.mailchimp.com/oauth2/metadata',
@@ -37,9 +46,68 @@ class Mailchimp {
             'headers' => [
                 'User-Agent' => 'oauth2-draft-v10',
                 'Accept'     => 'application/json',
-                'Authorization'      => 'OAuth ' . $apiKey
+                'Authorization'      => 'OAuth ' . $accessToken
             ]
         ]);
+
+        return json_decode($response->getBody()->getContents())->get;
+    }
+
+    public function getLists($accessToken)
+    {
+        $client = new Client([
+            'base_uri' => 'https://us20.api.mailchimp.com/3.0/',
+            'timeout'  => 2.0,
+        ]);
+
+        $response = $client->request('GET', 'lists', [
+            'headers' => [
+                'Authorization' => 'apikey ' . $accessToken
+            ]
+        ]);
+
+        return json_decode( $response->getBody()->getContents())->get;
+    }
+
+    public function getMembers()
+    {
+        $client = new Client([
+            'base_uri' => 'https://us20.api.mailchimp.com/3.0/',
+            'timeout'  => 0,
+        ]);
+        
+        // Get all members
+        $offset = 0;
+        $count = 1000;
+
+        do {
+
+            $response = $client->request('GET', 'lists/828d1335f5/members?offset=' . $offset . '&count=' . $count, [
+                'headers' => [
+                    'Authorization' => 'apikey ' . env('MAILCHIMP_API_KEY')
+                ]
+            ]);
+
+            $r = json_decode($response->getBody()->getContents())->members;
+
+            $total = count($r);
+
+            foreach ($r as $key) {
+                echo $key->email_address . "<br>";
+            }
+
+            $offset += 1000;
+            
+        } while ($total >= 1000);
+    }
+
+    protected function storeMembers()
+    {
+/*         DB::table('users')->insert([
+            ['email' => 'taylor@example.com'],
+            ['email' => 'dayle@example.com']
+        ]); */
+        // Store all members in DB
     }
 
 }
